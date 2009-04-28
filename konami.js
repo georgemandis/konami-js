741d6cb37b51afd8df002c2163c7e3d8a20b49c5
george.mandis@gmail.com <george.mandis@gmail.com@a62f2a58-33d0-11de-8c0b-2fbac4a05959>
2009-04-28 09:10:52 +0000

    Added the main javascript file.  Woop-dee-doo
    git-svn-id: https://konami-js.googlecode.com/svn/trunk@3 a62f2a58-33d0-11de-8c0b-2fbac4a05959

--- konami.js
+++ konami.js
@@ -7,3 +7,24 @@
  * http://dev.perl.org/licenses/
  * Tested in: Safari 4, Firefox 3, IE7
  */
+
+var konami = {
+	input:"",
+	clear:setTimeout('konami.clear_input()',2000),
+	load: function(link) {
+		window.document.onkeydown = function(e) {
+			konami.input+= e.keyCode	
+			if (konami.input == "38384040373937396566656613") {
+				konami.code(link)
+				clearTimeout(konami.clear)
+				}
+			clearTimeout(konami.clear)
+			konami.clear = setTimeout("konami.clear_input()",2000)
+			}
+	},
+	code: function(link) { window.location=link},
+	clear_input: function() {
+		konami.input="";
+		clearTimeout(konami.clear);
+	}
+}
