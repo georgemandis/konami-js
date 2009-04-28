d428a2cfee9567eab0d7a7d67106d0802d8e69f5
george.mandis@gmail.com <george.mandis@gmail.com@a62f2a58-33d0-11de-8c0b-2fbac4a05959>
2009-04-28 09:08:29 +0000

    Starting repo for konami-js
    git-svn-id: https://konami-js.googlecode.com/svn/trunk@2 a62f2a58-33d0-11de-8c0b-2fbac4a05959

---
+++ konami.js
@@ -0,0 +1,9 @@
+/*
+ * Konami-JS
+ * http://snaptortoise.com/projects/konami-js
+ * Copyright (c) 2009 George Mandis (georgemandis.com)
+ * Version: 1.0 (04/27/2009)
+ * Licensed under the Artistic License/GPL
+ * http://dev.perl.org/licenses/
+ * Tested in: Safari 4, Firefox 3, IE7
+ */
