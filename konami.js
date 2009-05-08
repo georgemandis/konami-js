d38d628c3a33fc395efab61326016d90b8be98bd
george.mandis@gmail.com <george.mandis@gmail.com@a62f2a58-33d0-11de-8c0b-2fbac4a05959>
2009-05-08 21:19:54 +0000

    Update date and version number in comments. I just like pushing changes.
    git-svn-id: https://konami-js.googlecode.com/svn/trunk@9 a62f2a58-33d0-11de-8c0b-2fbac4a05959

--- konami.js
+++ konami.js
@@ -2,7 +2,7 @@
  * Konami-JS
  * http://snaptortoise.com/konami-js
  * Copyright (c) 2009 George Mandis (georgemandis.com)
- * Version: 1.0 (04/27/2009)
+ * Version: 1.0.8 (05/08/2009)
  * Licensed under the Artistic License/GPL
  * http://dev.perl.org/licenses/
  * Tested in: Safari 4, Firefox 3, IE7
