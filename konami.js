409759496867e9fc240e641f999c5d93397cf153
george.mandis@gmail.com <george.mandis@gmail.com@a62f2a58-33d0-11de-8c0b-2fbac4a05959>
2010-02-04 09:44:08 +0000

    Updated to properly handle multiple instances of Konami-JS on the same page.
    git-svn-id: https://konami-js.googlecode.com/svn/trunk@14 a62f2a58-33d0-11de-8c0b-2fbac4a05959

--- konami.js
+++ konami.js
@@ -1,27 +1,43 @@
+
 /*
-	* Konami-JS ~ Now with iPhone support!
+	* Konami-JS ~ 
+	* :: Now with support for touch events and multiple instances for 
+	* :: situations that call for multiple easter eggs!
 	* Code: http://konami-js.googlecode.com/
 	* Examples: http://www.snaptortoise.com/konami-js
-	* Copyright (c) 2009 George Mandis (georgemandis.com)
-	* Version: 1.1 (12/17/2009)
+	* Copyright (c) 2009 George Mandis (georgemandis.com, snaptortoise.com)
+	* Version: 1.2 (1/30/2010)
 	* Licensed under the GNU General Public License v3
 	* http://www.gnu.org/copyleft/gpl.html
 	* Tested in: Safari 4, Firefox 3, IE7 and Mobile Safari 2.2.1
 */
 
+
 var Konami = function() {
 	var konami= {
+			addEvent:function ( obj, type, fn )
+			{
+				if (obj.addEventListener)
+					obj.addEventListener( type, fn, false );
+				else if (obj.attachEvent)
+				{
+					obj["e"+type+fn] = fn;
+					obj[type+fn] = function() { obj["e"+type+fn]( window.event ); }
+					obj.attachEvent( "on"+type, obj[type+fn] );
+				}
+			},
 	        input:"",
 	        pattern:"3838404037393739666513",
-	        load: function(link) {
-	                window.document.onkeydown = function(e) {
-	                        konami.input+= e ? e.keyCode : event.keyCode;
-	                        if (konami.input.indexOf(konami.pattern) != -1) {
-	                                konami.code(link);
-	                                return;
-	                                }
-	                        }
-	                        this.iphone.load(link)
+	        load: function(link) {			
+				konami.addEvent(document,"keydown", function(e) {						
+            	konami.input+= e ? e.keyCode : event.keyCode;
+            	if (konami.input.indexOf(konami.pattern) != -1) {
+                    konami.code(link);
+					konami.input="";
+                   	return;
+                    }
+            	});
+            this.iphone.load(link)
 	                },
 	        code: function(link) { window.location=link},
 	        iphone:{
@@ -34,7 +50,7 @@ var Konami = function() {
 	                keys:["UP","UP","DOWN","DOWN","LEFT","RIGHT","LEFT","RIGHT","TAP","TAP","TAP"],
 	                code: function(link) { window.location=link},
 	                load: function(link){
-	                        document.ontouchmove = function(e){
+	                        konami.addEvent(document,"touchmove",function(e){
 	                          if(e.touches.length == 1 && konami.iphone.capture==true){ 
 	                            var touch = e.touches[0]; 
 	                                konami.iphone.stop_x = touch.pageX;
@@ -43,16 +59,16 @@ var Konami = function() {
 	                                konami.iphone.capture=false;
 	                                konami.iphone.check_direction();
 	                                }
-	                                }               
-	                        document.ontouchend = function(evt){
+	                                });               
+	                        konami.addEvent(document,"touchend",function(evt){
 	                                if (konami.iphone.tap==true) konami.iphone.check_direction();           
-	                                }
-	                        document.ontouchstart = function(evt){
+	                                },false);
+	                        konami.addEvent(document,"touchstart", function(evt){
 	                                konami.iphone.start_x = evt.changedTouches[0].pageX
 	                                konami.iphone.start_y = evt.changedTouches[0].pageY
 	                                konami.iphone.tap = true
 	                                konami.iphone.capture = true
-	                                }               
+	                                });               
 	                                },
 	                check_direction: function(){
 	                        x_magnitude = Math.abs(this.start_x-this.stop_x)
@@ -68,3 +84,4 @@ var Konami = function() {
 	}
 	return konami;
 }
+
