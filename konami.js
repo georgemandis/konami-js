47d2250c1dd00b6a7152ee4d92fafc4c671abb58
George Mandis <george@mand.is>
2021-11-11 12:33:28 -0800

    Updating old github refs

--- konami.js
+++ konami.js
@@ -2,8 +2,8 @@
  * Konami-JS ~
  * :: Now with support for touch events and multiple instances for
  * :: those situations that call for multiple easter eggs!
- * Code: https://github.com/snaptortoise/konami-js
- * Copyright (c) 2009 George Mandis (georgemandis.com, snaptortoise.com)
+ * Code: https://github.com/georgemandis/konami-js
+ * Copyright (c) 2009 George Mandis (https://george.mand.is)
  * Version: 1.6.2 (7/17/2018)
  * Licensed under the MIT License (http://opensource.org/licenses/MIT)
  * Tested in: Safari 4+, Google Chrome 4+, Firefox 3+, IE7+, Mobile Safari 2.2.1+ and Android
