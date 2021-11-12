fba71a7d25790d6e43a0ed5bbd9580afb544befe
George Mandis <george@mand.is>
2021-11-11 20:58:38 -0800

    No favicon for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  // favicon: 'img/favicon.ico',
   organizationName: 'georgemandis', // Usually your GitHub org/user name.
   projectName: 'konami-js', // Usually your repo name.
   deploymentBranch: "docs",
