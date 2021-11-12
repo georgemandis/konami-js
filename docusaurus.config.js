becb9d0b3dcfd493d1b7237b5f27ab32f4e231af
George Mandis <george@mand.is>
2021-11-11 21:04:22 -0800

    Trailing slash change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  trailingSlash: true,
+  trailingSlash: false,
   // favicon: 'img/favicon.ico',
   organizationName: 'georgemandis', // Usually your GitHub org/user name.
   projectName: 'konami-js', // Usually your repo name.
